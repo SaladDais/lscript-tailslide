f0a73283856194835207db9356ea182ae52d104e
Salad Dais <SaladDais@users.noreply.github.com>
2022-02-12 21:39:51 +0000

    Compile and execute passed-in script in harness

--- indra/lscript/lscript_harness/lscriptharness.cpp
+++ indra/lscript/lscript_harness/lscriptharness.cpp
@@ -1,9 +1,15 @@
 #include "linden_common.h"
+#include "llerrorcontrol.h"
+
 #include "../lscript_rt_interface.h"
 
 int main(int argc, char **argv) {
 	if (argc != 2)
 		return 1;
-	return !lscript_compile(argv[1], "/dev/stdout", "/dev/stderr", 0, "foo", 0);
+	LLError::initForApplication(".", ".", true /* log to stderr */);
+	if(!lscript_compile(argv[1], "/tmp/whatever.lso2", "/dev/stderr", 0, "foo", 0))
+		return 1;
+	lscript_run("/tmp/whatever.lso2", FALSE);
+	return 0;
 }
 
