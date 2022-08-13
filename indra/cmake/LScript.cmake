b3a715136991cadfccebac4a67c82ca07e500347
Salad Dais <SaladDais@users.noreply.github.com>
2022-08-13 12:48:27 +0000

    Use autobuild package for tailslide on linux64

--- indra/cmake/LScript.cmake
+++ indra/cmake/LScript.cmake
@@ -1,5 +1,8 @@
 # -*- cmake -*-
 
+include(00-Common)
+include(Prebuilt)
+
 set(LSCRIPT_INCLUDE_DIRS
     ${LIBS_OPEN_DIR}/lscript
     ${LIBS_OPEN_DIR}/lscript/lscript_compile
@@ -12,5 +15,9 @@ set(LSCRIPT_LIBRARIES
     lscript_execute    
     lscript_library
     )
+
+if(LINUX)
+    use_prebuilt_binary(tailslide)
+endif()
 
 set(LSCRIPT_EXECUTE_MONO_LIBRARIES lscript_execute_mono)
